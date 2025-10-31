369bbd983c15b5af0bf951ca0f71cd6f39ae176c
synoize <suryavanshivam@gamil.com>
2025-10-31 17:48:58 +0530

    update cors

--- server/server.js
+++ server/server.js
@@ -14,8 +14,9 @@ const corsOptions = {
     origin: [
         "http://localhost:5174",
         "http://localhost:5173",
-        "https://appliancesakhi.in",
-        "https://www.appliancesakhi.in"
+        "https://zscircle.vercel.app",
+        "https://zscircle.tech",
+        "https://www.zscircle.tech"
     ],
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
